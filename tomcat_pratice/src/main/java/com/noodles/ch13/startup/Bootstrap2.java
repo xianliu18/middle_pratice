c300185ecbdbe53f14c4f2185f739acde4387dbc
liuxian <liuxian@xhlmarketing.com>
2023-11-28 12:01:06 +0800

    standardEngine 应用

---
+++ tomcat_pratice/src/main/java/com/noodles/ch13/startup/Bootstrap2.java
@@ -0,0 +1,77 @@
+package com.noodles.ch13.startup;
+
+import com.noodles.ch13.core.SimpleContextConfig;
+import org.apache.catalina.Connector;
+import org.apache.catalina.Context;
+import org.apache.catalina.Engine;
+import org.apache.catalina.Host;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.Loader;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardEngine;
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.core.StandardWrapper;
+import org.apache.catalina.loader.WebappLoader;
+
+import java.io.File;
+
+/**
+ * @Description 启动类
+ * @Author liuxian
+ * @Date 2023/11/28 11:41
+ **/
+public class Bootstrap2 {
+
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "tomcat-webroot";
+
+    public static void main(String[] args) {
+        System.setProperty("catalina.base", WEB_ROOT);
+        Connector connector = new HttpConnector();
+
+        Wrapper wrapper1 = new StandardWrapper();
+        wrapper1.setName("Primitive");
+        wrapper1.setServletClass("PrimitiveServlet");
+        Wrapper wrapper2 = new StandardWrapper();
+        wrapper2.setName("Modern");
+        wrapper2.setServletClass("ModernServlet");
+
+        Context context = new StandardContext();
+        context.setPath("/app1");
+        context.setDocBase("app1");
+
+        context.addChild(wrapper1);
+        context.addChild(wrapper2);
+
+        LifecycleListener listener = new SimpleContextConfig();
+        ((Lifecycle)context).addLifecycleListener(listener);
+
+        Host host = new StandardHost();
+        host.addChild(context);
+        host.setName("localhost");
+        host.setAppBase("webapps");
+
+        Loader loader = new WebappLoader();
+        context.setLoader(loader);
+        context.addServletMapping("/Primitive", "Primitive");
+        context.addServletMapping("/Modern", "Modern");
+
+        Engine engine = new StandardEngine();
+        engine.addChild(host);
+        engine.setDefaultHost("localhost");
+
+        connector.setContainer(engine);
+        try {
+            connector.initialize();
+            ((Lifecycle)connector).start();
+            ((Lifecycle)engine).start();
+
+            System.in.read();
+            ((Lifecycle)engine).stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
